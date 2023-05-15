e59a9316635366e1ad37476f5d2b2de3d0e5cd47
Aakarshashetty <aakarshakodthiwada61121@gmail.com>
2023-05-15 11:52:17 +0530

    initial commit

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -8,23 +8,65 @@ import { v4 as uuid } from "uuid";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
+    name: "Lipstick",
+    brand: "Maybelline",
+    category: "Lips",
+    description: "A long-lasting, creamy lipstick in a variety of shades.",
+    price: 10.99,
+    image: "https://example.com/lipstick.jpg",
+    reviews: [
+      {
+        username: "User1",
+        rating: 4,
+        comment: "I love this lipstick! The color is perfect for me.",
+      },
+      {
+        username: "User2",
+        rating: 5,
+        comment: "This is the best lipstick I have ever used.",
+      },
+    ],
   },
   {
     _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
+    name: "Mascara",
+    brand: "L'Oreal",
+    category: "Eyes",
+    description: "A volumizing mascara that adds length and definition.",
+    price: 12.99,
+    image: "https://example.com/mascara.jpg",
+    reviews: [
+      {
+        username: "User3",
+        rating: 3,
+        comment: "This mascara is good, but it smudges easily.",
+      },
+      {
+        username: "User4",
+        rating: 4,
+        comment: "I like the way this mascara makes my lashes look.",
+      },
+    ],
   },
   {
     _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
+    name: "Foundation",
+    brand: "MAC",
+    category: "Face",
+    description: "A full-coverage foundation that lasts all day.",
+    price: 25.99,
+    image: "https://example.com/lipstick.jpg",
+    reviews: [
+      {
+        username: "User5",
+        rating: 5,
+        comment: "This foundation is amazing! It covers all my blemishes.",
+      },
+      {
+        username: "User6",
+        rating: 2,
+        comment: "I didn't like this foundation. It made my skin look oily.",
+      },
+    ],
   },
 ];
